f1b9f04f9eb897892733f91366c09f87686a67eb
SatyamV7 <satyamverma46@outlook.com>
2022-06-14 18:09:43 +0530

    nothing

--- serviceworker.js
+++ serviceworker.js
@@ -1,14 +1,86 @@
-// install service worker
-self.addEventListener('install', evt => {
+/*
+ Copyright SatyamV7. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+// Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
+const PRECACHE = 'Static_Cache';
+const RUNTIME = 'Dynamic_Cache';
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "/Fallback/fallback.html";
+
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+  '/',
+  'index.html',
+  '/icons/favicon.ico',
+  '/About_Me/index.html',
+  '/About_Me/Social/phone.svg',
+  '/About_Me/Social/mail.svg',
+  '/About_Me/Social/github.svg',
+  '/Sign_in/index.html',
+  '/Sign_in/avtar.png',
+  '/Fallback/fallback.html',
+];
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
     console.log('service worker has been installed');
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
 });
 
-// activate service worker
-self.addEventListener('activate', evt => {
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
     console.log('service worker has been activated');
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
 });
 
-//fetch event
-self.addEventListener('fetch', evt => {
-    console.log('fetch event', evt);
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+    console.log('fetch event');
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
 });
