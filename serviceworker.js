f88df872a7bc9eca39b33f5bca1ca418806bdc1a
SatyamV7 <satyamverma46@outlook.com>
2022-07-02 19:33:39 +0530

    update about_me/index.html

--- serviceworker.js
+++ serviceworker.js
@@ -14,51 +14,51 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'Static_Cache v2';
-const RUNTIME = 'Dynamic_Cache v2';
+const PRECACHE = 'Static_Cache v3';
+const RUNTIME = 'Dynamic_Cache v3';
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "/Fallback/fallback.html";
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  '/',
-  'index.html',
-  '/icons/favicon.ico',
-  '/About_Me/index.html',
-  '/About_Me/Social/phone.svg',
-  '/About_Me/Social/mail.svg',
-  '/About_Me/Social/github.svg',
-  '/Sign_in/index.html',
-  '/Sign_in/avtar.png',
-  '/Fallback/fallback.html',
-  'https://fonts.googleapis.com/css2?family=Nunito&display=swap',
-  'https://fonts.gstatic.com/s/nunito/v24/XRXI3I6Li01BKofiOc5wtlZ2di8HDLshdTQ3jw.woff2',
+    '/',
+    'index.html',
+    '/icons/favicon.ico',
+    '/About_Me/index.html',
+    '/About_Me/Social/phone.svg',
+    '/About_Me/Social/mail.svg',
+    '/About_Me/Social/github.svg',
+    '/Sign_in/index.html',
+    '/Sign_in/avtar.png',
+    '/Fallback/fallback.html',
+    'https://fonts.googleapis.com/css2?family=Nunito&display=swap',
+    'https://fonts.gstatic.com/s/nunito/v24/XRXI3I6Li01BKofiOc5wtlZ2di8HDLshdTQ3jw.woff2',
 ];
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
     console.log('service worker has been installed');
-  event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
+    event.waitUntil(
+        caches.open(PRECACHE)
+            .then(cache => cache.addAll(PRECACHE_URLS))
+            .then(self.skipWaiting())
+    );
 });
 
 // The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', event => {
     console.log('service worker has been activated');
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+        }).then(cachesToDelete => {
+            return Promise.all(cachesToDelete.map(cacheToDelete => {
+                return caches.delete(cacheToDelete);
+            }));
+        }).then(() => self.clients.claim())
+    );
 });
 
 // The fetch handler serves responses for same-origin resources from a cache.
@@ -66,23 +66,23 @@ self.addEventListener('activate', event => {
 // from the network before returning it to the page.
 self.addEventListener('fetch', event => {
     console.log('fetch event');
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+    // Skip cross-origin requests, like those for Google Analytics.
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
 
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
+                return caches.open(RUNTIME).then(cache => {
+                    return fetch(event.request).then(response => {
+                        // Put a copy of the response in the runtime cache.
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
+    }
 });
